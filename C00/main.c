9141095f40e2aaba43e61459c2bb56b3accd47dc
Alyssa VACHERON <avachero@e3r1p3.42.fr>
2021-06-30 18:13:54 +0200

    av

---
+++ C00/main.c
@@ -0,0 +1,19 @@
+
+//void ft_putchar(char c);
+//void ft_print_alphabet(void);
+//void ft_print_reverse_alphabet(void);
+//void ft_print_numbers(void);
+//void ft_is_negative(int n);
+//void ft_print_comb(void);
+void ft_print_comb2(void);
+int main()
+{
+    //ft_putchar('c');
+    //ft_print_alphabet();
+   //ft_print_reverse_alphabet();
+   //ft_print_numbers();
+   //ft_is_negative(5);
+  //ft_print_comb();
+  ft_print_comb2();
+    return 0;
+}
