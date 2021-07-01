cabc2ff28c815a913ef513576e651ac99af3a55f
Alyssa VACHERON <avachero@e2r1p5.42.fr>
2021-07-01 13:35:09 +0200

    fini

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,1 +1,3 @@
- cat /etc/passwd | sed '/^#d' | awk 'NR$2' | cut -d : -f 1 | rev |  sort -r  | awk "NR>=$FT_LINE1 && NR<=$FT_LINE2" | tr '\n' ' ' | sed '$ s/ $/./' | sed 's/ /, /g' | tr -d '\n'
+ cat /etc/passwd | grep -v '#' | awk 'NR%2==0' | cut -d : -f 1 | rev | sort -r | awk -v line_one="$FT_LINE1" -v line_two="$FT_LINE2" 'NR==line_one,NR==line_two'  | tr '\n' ' ' | sed '$ s/ $/./' | sed 's/ /, /g' | tr -d '\n'
+
+
