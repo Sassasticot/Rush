efc637bf4b152062cd7d3dfe019fb3b8784387d0
Sassasticot <86429066+Sassasticot@users.noreply.github.com>
2021-07-04 12:10:06 +0200

    Update rush000.c

--- RUSH/rush000.c
+++ RUSH/rush000.c
@@ -44,3 +44,75 @@ int main()
 {
 	rush(1, 5);
 }
+
+
+
+void rush(int x, int y)
+{
+    int a;
+    int b;
+
+    a =1;
+    while(a <= y)
+    {
+        if (a == 1 )
+            write(1, "/", 1);
+         if (a == y)
+            write(1, "\\", 1);
+
+        b= 1;
+        while(b <= x)
+        {   if (b == x-2 && (a == y || a == 1))
+            b = x+1;
+            if ((b == 1 || b == x) || (x>1 && (a == 1 || a ==y)))
+                    write(1, "*", 1);
+            else
+                    write(1, " ", 1);
+            b++;
+        }
+        if (a == 1 && x > 1)
+            write(1, "\\", 1);
+         if (a == y && x > 1)
+            write(1, "/", 1);
+        write(1, "\n", 1);
+        a++;
+    }
+}
+
+
+#include <unistd.h>
+
+
+void rush(int x, int y)
+{
+    int a;
+    int b;
+
+    a =1;
+    while(a <= y)
+    {
+        if (a == 1 )
+            write(1, "/", 1);
+        else if (a == y)
+            write(1, "\\", 1);
+            else
+                write(1, "*", 1);
+        b= 1;
+        while(b <= x-2)
+        {
+            if (a == 1 || a ==y)
+                    write(1, "*", 1);
+            else
+                    write(1, " ", 1);
+            b++;
+        }
+        if (a == 1 && x > 1)
+            write(1, "\\", 1);
+        else if (a == y && x > 1)
+            write(1, "/", 1);
+        else if (x > 1)
+            write(1, "*", 1);
+        write(1, "\n", 1);
+        a++;
+    }
+}
